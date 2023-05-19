514d2c97fd0357bd575d77bf72fe27fe1954f762
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-05-19 18:50:41 -0400

    finished assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+class Vehicle{
+    constructor(model){
+        this.model = model
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
+
+class Sedan extends Vehicle{
+    constructor(model, backupCamera){
+        super(model)
+        this.model = model
+        this.backupCamera = backupCamera
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
+
+class Suv extends Vehicle{
+    constructor(model,offroadPackage){
+        super(model)
+        this.model = model
+        this.offroadPackage = offroadPackage
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
+
+class Truck extends Vehicle{
+    constructor(model,towingCapacity){
+        super(model)
+        this.model = model
+        this.towingCapacity = towingCapacity
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
+
+class EVSedan extends Vehicle{
+    constructor(model,backupCamera){
+        super(model,backupCamera)
+        this.model = model
+        this.backupCamera = backupCamera
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+    recharge(){
+        return `${this.model} recharges`
+    }
+}
+
+
+
